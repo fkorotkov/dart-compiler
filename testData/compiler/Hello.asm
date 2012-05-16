30afa0f1a51b164c9d7f8753f56b0a2aff519047
Fedor Korotkov <fedor.korotkov@odnoklassniki.ru>
2012-05-17 00:18:49 +0400

    simplify

--- testData/compiler/Hello.asm
+++ testData/compiler/Hello.asm
@@ -17,27 +17,9 @@ _main:
  
         sub  esp, 4 ; align the stack
         push dword hello_world_str
-        call _puts
-        add  esp, 4
+        call _printf
 
-        ; malloc 16 bytes
-        push  dword 16
-        call  _malloc
-        add  esp, 4
-
-        ; check if the malloc failed
-        test  eax, eax
-        jz    fail_exit
-
-        ; free the malloc'd memory
-        push  ebx
-        call  _free
-        add  esp, 4
         add  esp, 4 ; cleanup the stack
-        pop  ebp
-        ret
-
-fail_exit:
-        mov  eax, 1
+        mov  esp, ebp
         pop  ebp
         ret
